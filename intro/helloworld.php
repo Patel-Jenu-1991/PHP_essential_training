bc1ebb27a8bd9a5d03f03092da1a1535078cc552
Jenu Patel <jenu.16@icloud.com>
2017-07-07 12:55:15 -0500

    Output dynamic text with php

---
+++ intro/helloworld.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php echo "Hello World!"; ?>
+        <br />
+        <?php
+            // concatenating strings with
+            // a dot operator
+            echo "Hello" . " World!";
+        ?>
+        <br />
+        <?php
+            // echo is essential to display output
+            // php can generate numbers
+            echo 2 + 3;
+        ?>
+    </body>
+</html>
