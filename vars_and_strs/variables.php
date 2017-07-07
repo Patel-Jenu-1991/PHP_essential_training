d850def0fbb1a1cca07b097b60b579459a3baf52
Jenu Patel <jenu.16@icloud.com>
2017-07-07 13:32:01 -0500

    working with variables in PHP

---
+++ vars_and_strs/variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Variables in PHP</title>
+    </head>
+    <body>
+        <?php
+            // variable names begin with a
+            // dollar sign in PHP
+            $var1 = 10;
+            echo $var1;
+
+            echo "<br />";
+
+            $var1 = 100;    // re-assigning the value of var1
+            echo $var1;
+
+            echo "<br />";
+
+            $var2 = "Hello World";
+            echo $var2;
+        ?>
+    </body>
+</html>
