05b575c9a5e6b399ddbbbc8e00fc9bc0fdb6b5e7
Jenu Patel <jenu.16@icloud.com>
2017-07-07 13:15:42 -0500

    Using comments in PHP

---
+++ intro/comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Comments in PHP</title>
+    </head>
+    <body>
+        <?php echo "Using comments in PHP" ?>
+        <?php
+            // single-line comments are like this
+
+            # or like this (less common)
+
+            /*
+                Multi line comments are written
+                like this, so that you can keep
+                typing and typing
+            */
+        ?>
+    </body>
+</html>
